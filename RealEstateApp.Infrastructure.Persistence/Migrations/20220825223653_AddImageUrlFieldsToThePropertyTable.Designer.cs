7966bf9f3268c63d1f44a816d3e531da7ddd3ce1
Adrian Mota <adrianmota152@gmail.com>
2022-08-25 19:02:27 -0400

    Adds new migration and fixes some of the upgrades' functionalities

--- RealEstateApp.Infrastructure.Persistence/Migrations/20220825223653_AddImageUrlFieldsToThePropertyTable.Designer.cs
+++ RealEstateApp.Infrastructure.Persistence/Migrations/20220825223653_AddImageUrlFieldsToThePropertyTable.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RealEstateApp.Infrastructure.Persistence.Contexts;
 
 namespace RealEstateApp.Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(RealEstateContext))]
-    partial class RealEstateContextModelSnapshot : ModelSnapshot
+    [Migration("20220825223653_AddImageUrlFieldsToThePropertyTable")]
+    partial class AddImageUrlFieldsToThePropertyTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ RealEstateApp.Infrastructure.Persistence/Migrations/20220825223653_AddImageUrlFieldsToThePropertyTable.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RealEstateApp.Infrastructure.Persistence.Migrations
+{
+    public partial class AddImageUrlFieldsToThePropertyTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl1",
+                table: "Properties",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl2",
+                table: "Properties",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl3",
+                table: "Properties",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl4",
+                table: "Properties",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "Favorites",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageUrl1",
+                table: "Properties");
+
+            migrationBuilder.DropColumn(
+                name: "ImageUrl2",
+                table: "Properties");
+
+            migrationBuilder.DropColumn(
+                name: "ImageUrl3",
+                table: "Properties");
+
+            migrationBuilder.DropColumn(
+                name: "ImageUrl4",
+                table: "Properties");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UserId",
+                table: "Favorites",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
