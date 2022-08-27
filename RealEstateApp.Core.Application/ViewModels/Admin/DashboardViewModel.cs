65c2c3d2bcc823f85516efca241da04f76e82539
JoelGrullon12 <jjj007507388@gmail.com>
2022-08-27 04:27:24 -0400

    Admin dashboard added

---
+++ RealEstateApp.Core.Application/ViewModels/Admin/DashboardViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealEstateApp.Core.Application.ViewModels.Admin
+{
+    public class DashboardViewModel
+    {
+        public int Properties { get; set; }
+        public int ActiveAgents { get; set; }
+        public int InactiveAgents { get; set; }
+        public int ActiveClients { get; set; }
+        public int InactiveClients { get; set; }
+        public int ActiveDevelopers { get; set; }
+        public int InactiveDevelopers { get; set; }
+    }
+}
